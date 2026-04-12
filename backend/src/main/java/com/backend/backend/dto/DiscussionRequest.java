228279ea1572d39cbaa5a4d2146f4b1ab339cd46
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-04-12 15:11:48 +0600

    update DiscussionService

--- backend/src/main/java/com/backend/backend/dto/DiscussionRequest.java
+++ backend/src/main/java/com/backend/backend/dto/DiscussionRequest.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DiscussionRequest{
     String description;
-    String roomNo;
-    String userId;
+    Integer roomNo;
+    Integer userId;
 }
