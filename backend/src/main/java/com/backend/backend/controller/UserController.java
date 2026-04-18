879255c30f6553d57d684505fc6d3d31c3e55c63
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-04-18 18:32:49 +0600

    update application-dev.properties

--- backend/src/main/java/com/backend/backend/controller/UserController.java
+++ backend/src/main/java/com/backend/backend/controller/UserController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -73,4 +75,18 @@ public class UserController {
             }
     
         }  
+
+
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<?> getUsers(
+                ) {
+            try {
+                return ResponseEntity.status(HttpStatus.OK).body(userService.getUsers());
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            }
+    
+        }
 }
--- backend/src/main/java/com/backend/backend/dto/UserRequest.java
+++ backend/src/main/java/com/backend/backend/dto/UserRequest.java
@@ -1,5 +1,6 @@
 package com.backend.backend.dto;
 
+import java.time.LocalDate;
 import java.util.List;
 
 
@@ -12,6 +13,21 @@ public class UserRequest {
     @NotEmpty(message = "usename must be required")
     @Size(max = 7,min = 3,message = "username size max 7 characters and min 3 characters")
     private String userName;
+    @NotEmpty(message = "first name must be required")
+    private String firstName;
+    @NotEmpty(message = "last name must be required")
+    private String lastName;
+    @NotEmpty(message = "email must be required")
+    private String email;
+    @NotEmpty(message = "contact number must be required")
+    private String contactNo;
+    @NotEmpty(message = "emergency contact no must be required")
+    private String emergencyContactNo;
+    
+    private LocalDate birthDate;
+    @NotEmpty(message = "permenant address must be required")
+    private String permanentAddress;
+    private String passportId;
     @NotEmpty(message = "password must be required")
     @Size(max = 14,min = 4,message = "password size max 14 characters and min 4 character")
     private String password;
